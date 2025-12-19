4cd96fb41d39cde91c0bddbbd3ae4a6826edf972
Nancy Ashraf <nancy.sabry05@eng-st.cu.edu.eg>
2025-12-19 14:27:32 +0200

    Edited WB_stage

--- CPU_v0.9067/WB_Stage.vhd
+++ CPU_v0.9067/WB_Stage.vhd
@@ -4,21 +4,19 @@ USE ieee.numeric_std.ALL;
 
 ENTITY WB_Stage IS
     PORT (
-        WBSelWB      : IN STD_LOGIC;
+        WBSelWB      : IN STD_LOGIC := '0';
         
-        MemOutWB     : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-        EXOutWB      : IN STD_LOGIC_VECTOR(31 DOWNTO 0); 
-
-        RegDataWB    : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
-);
+        MemOutWB     : IN STD_LOGIC_VECTOR(31 DOWNTO 0) := x"0000_0000"; 
+        EXOutWB      : IN STD_LOGIC_VECTOR(31 DOWNTO 0) := x"0000_0000"; 
+        RegDataWB    : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) := x"0000_0000"
+    );
 END ENTITY;
 
 ARCHITECTURE Behavioral OF WB_Stage IS
-
 BEGIN
-    
     WITH WBSelWB SELECT
-        RegDataWB <= EXOutWB   WHEN "0",
-                     MemOutWB  WHEN "1";
-
+        RegDataWB <= EXOutWB   WHEN '0',
+                     MemOutWB  WHEN '1',
+                     x"0000_0000" WHEN OTHERS;
+                     
 END ARCHITECTURE;
